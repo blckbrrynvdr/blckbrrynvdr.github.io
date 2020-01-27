bb2e1673b9e4a24312184331160f21a556663dcd
blckbrrynvdr <43585788+blckbrrynvdr@users.noreply.github.com>
2020-01-27 11:05:43 +0300

    9798796

--- sender.js
+++ sender.js
@@ -1,10 +1,13 @@
 $(function() {
     $('#send').on('click', function () {
-        var url = 'fcm.googleapis.com';
+        var url = 'https://fcm.googleapis.com/fcm/send';
         $.ajax({
             type: 'POST',
             url: url,
-            Authorization: key='AAAA6w1WEXE:APA91bEi_7jXUxn4h3ou-kXUaMWcnpRDmmx7nuL1_pOrjzL4BzuUWzjfV6Liv0ufJLTMfyDd2RJ1_NBRH0Dp74GO0Ud1n08aebHoYAWqpZ9wptvkuQISQxcYaokrS8C7Vpr2EnittJL4',
+            headers: {
+                Authorization: key='AAAA6w1WEXE:APA91bEi_7jXUxn4h3ou-kXUaMWcnpRDmmx7nuL1_pOrjzL4BzuUWzjfV6Liv0ufJLTMfyDd2RJ1_NBRH0Dp74GO0Ud1n08aebHoYAWqpZ9wptvkuQISQxcYaokrS8C7Vpr2EnittJL4'
+            },
+            contentType: 'application/json',
             data: {
                 "notification": {
                     "title": "Ералаш",
@@ -12,7 +15,7 @@ $(function() {
                     "icon": "https://eralash.ru.rsz.io/sites/all/themes/eralash_v5/logo.png?width=40&height=40",
                     "click_action": "http://eralash.ru/"
                 },
-                "to": "dFIpkrMbJdk:APA91bF9R-UviaR7sJ5EBwHdwV0JZ_0bBL2dlAp6mIfb6fzXpjsYu68roAIq_5MbOpEFv_BNjGql3AqrUKu18qYst0emQuWbDlgBA82J8G3WJIhARsGUZzZTywaElpmTOA_yhAR_q2-n"
+                "to": $('#to')
             }
         })
     })
