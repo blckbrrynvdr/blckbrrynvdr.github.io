e683c5a9f7ca6f17f694e30fe7b54b591f3a06e7
blckbrrynvdr <43585788+blckbrrynvdr@users.noreply.github.com>
2020-01-27 09:30:02 +0300

    gdggggg

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '1009541058929'
+    messagingSenderId: 1009541058929
 });
 
 const messaging = firebase.messaging();
