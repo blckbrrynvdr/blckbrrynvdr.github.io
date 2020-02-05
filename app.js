6f1970c42baa2138f65b6b1d9d1d9844e32fa9c1
blckbrrynvdr <43585788+blckbrrynvdr@users.noreply.github.com>
2020-02-05 12:28:07 +0300

    v3.0.0.8

--- app.js
+++ app.js
@@ -4,88 +4,85 @@ firebase.initializeApp({
 
 var timeToShowSubscribe = 1000;
 
-console.log('fdkhoihoi')
-var messaging = firebase.messaging();
-getToken();
-// if (
-//     'Notification' in window &&
-//     'serviceWorker' in navigator &&
-//     'localStorage' in window &&
-//     'fetch' in window &&
-//     'postMessage' in window
-// ) {
-//
-//     var messaging = firebase.messaging();
-//
-//     // если уже есть разрешение на показ
-//     if (Notification.permission === 'granted') {
-//         getToken();
-//     }
-//
-//     // on ready показываем запрос на подписку через 10 секунд после загрузки документа
-//     $(document).ready(function () {
-//         setTimeout(function () {
-//             getToken()
-//         }, timeToShowSubscribe);
-//
-//     });
-//
-//     // обработчик прихода уведомлений с сервера
-//     messaging.onMessage(function (payload) {
-//         console.log('Message received', payload);
-//
-//         // регистрируем фейк сервис воркера для показа на мобилках
-//         navigator.serviceWorker.register('firebase-messaging-sw.js');
-//         Notification.requestPermission(function (permission) {
-//             if (permission === 'granted') {
-//                 navigator.serviceWorker.ready.then(function (registration) {
-//                     // Копируем объект данных для получения обработчика клика на уведомление
-//                     payload.data.data = JSON.parse(JSON.stringify(payload.data));
-//
-//                     registration.showNotification(payload.data.title, payload.data);
-//                 }).catch(function (error) {
-//                     // если регистрация сервис воркера не удалась
-//                     showError('ServiceWorker registration failed', error);
-//                 });
-//             }
-//         });
-//     });
-//
-//     // Колбэк при обновлении токена
-//     messaging.onTokenRefresh(function () {
-//         messaging.getToken()
-//             .then(function (refreshedToken) {
-//                 console.log('Token refreshed');
-//                 // Отправить токен на сервер
-//                 sendTokenToServer(refreshedToken);
-//             })
-//             .catch(function (error) {
-//                 showError('Unable to retrieve refreshed token', error);
-//             });
-//     });
-//
-// } else {
-//     if (!('Notification' in window)) {
-//         showError('Notification not supported');
-//     } else if (!('serviceWorker' in navigator)) {
-//         showError('ServiceWorker not supported');
-//     } else if (!('localStorage' in window)) {
-//         showError('LocalStorage not supported');
-//     } else if (!('fetch' in window)) {
-//         showError('fetch not supported');
-//     } else if (!('postMessage' in window)) {
-//         showError('postMessage not supported');
-//     }
-//
-//     console.warn('This browser does not support desktop notification.');
-//     console.log('Is HTTPS', window.location.protocol === 'https:');
-//     console.log('Support Notification', 'Notification' in window);
-//     console.log('Support ServiceWorker', 'serviceWorker' in navigator);
-//     console.log('Support LocalStorage', 'localStorage' in window);
-//     console.log('Support fetch', 'fetch' in window);
-//     console.log('Support postMessage', 'postMessage' in window);
-//
-// }
+if (
+    'Notification' in window &&
+    'serviceWorker' in navigator &&
+    'localStorage' in window &&
+    'fetch' in window &&
+    'postMessage' in window
+) {
+
+    var messaging = firebase.messaging();
+
+    // если уже есть разрешение на показ
+    if (Notification.permission === 'granted') {
+        getToken();
+    }
+
+    // on ready показываем запрос на подписку через 10 секунд после загрузки документа
+    $(document).ready(function () {
+        setTimeout(function () {
+            getToken()
+        }, timeToShowSubscribe);
+
+    });
+
+    // обработчик прихода уведомлений с сервера
+    messaging.onMessage(function (payload) {
+        console.log('Message received', payload);
+
+        // регистрируем фейк сервис воркера для показа на мобилках
+        navigator.serviceWorker.register('firebase-messaging-sw.js');
+        Notification.requestPermission(function (permission) {
+            if (permission === 'granted') {
+                navigator.serviceWorker.ready.then(function (registration) {
+                    // Копируем объект данных для получения обработчика клика на уведомление
+                    payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+                    registration.showNotification(payload.data.title, payload.data);
+                }).catch(function (error) {
+                    // если регистрация сервис воркера не удалась
+                    showError('ServiceWorker registration failed', error);
+                });
+            }
+        });
+    });
+
+    // Колбэк при обновлении токена
+    messaging.onTokenRefresh(function () {
+        messaging.getToken()
+            .then(function (refreshedToken) {
+                console.log('Token refreshed');
+                // Отправить токен на сервер
+                sendTokenToServer(refreshedToken);
+            })
+            .catch(function (error) {
+                showError('Unable to retrieve refreshed token', error);
+            });
+    });
+
+} else {
+    if (!('Notification' in window)) {
+        showError('Notification not supported');
+    } else if (!('serviceWorker' in navigator)) {
+        showError('ServiceWorker not supported');
+    } else if (!('localStorage' in window)) {
+        showError('LocalStorage not supported');
+    } else if (!('fetch' in window)) {
+        showError('fetch not supported');
+    } else if (!('postMessage' in window)) {
+        showError('postMessage not supported');
+    }
+
+    console.warn('This browser does not support desktop notification.');
+    console.log('Is HTTPS', window.location.protocol === 'https:');
+    console.log('Support Notification', 'Notification' in window);
+    console.log('Support ServiceWorker', 'serviceWorker' in navigator);
+    console.log('Support LocalStorage', 'localStorage' in window);
+    console.log('Support fetch', 'fetch' in window);
+    console.log('Support postMessage', 'postMessage' in window);
+
+}
 
 // функция получения токена
 function getToken() {
